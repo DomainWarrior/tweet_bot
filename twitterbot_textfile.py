d1c010b5f96a0a2d1ac5f6a09fb13cfd557b52c5
Domain Warrior <45982363+DomainWarrior@users.noreply.github.com>
2021-09-09 12:16:02 -0400

    Tweet every 15 minutes with this file, and facts.txt
    be sure to change facts.txt to whatever strings you want to use, keep in mind 140 max character limit on twitter and the hashtags need to follow the strings or the line will be commented out.

--- twitterbot_textfile.py
+++ twitterbot_textfile.py
@@ -4,16 +4,18 @@ from time import sleep
 import credentials
 from credentials import *
 
+#calling credentials and importing the keys
 auth = tweepy.OAuthHandler(credentials.API_key, credentials.API_secret_key)
 auth.set_access_token(credentials.access_token, credentials.access_token_secret)
 api = tweepy.API(auth)
 
+#this is where the file is being opened read and closed for the tweets being posted
 my_file = open('fact.txt', 'r', encoding="utf8")
 file_lines = my_file.readlines()
 my_file.close()
 
 
-# Tweet a line every 15 minutes
+# Tweet a line every 15 minutes change timer to make it longer or shorter or create another command and change it to make one for the hour!
 def tweet():
     for line in file_lines:
         try:
