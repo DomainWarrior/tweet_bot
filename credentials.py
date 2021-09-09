9e4370ba3389989ec0ce1143dacc0dd4957a8fd6
Domain Warrior <45982363+DomainWarrior@users.noreply.github.com>
2021-09-09 12:24:04 -0400

    Secret keys from twitter development
    be sure to change the strings (strings are " ")

--- credentials.py
+++ credentials.py
@@ -6,3 +6,4 @@ access_token = "access_token"
 access_token_secret = "access_token_secret"
 API_key = "API_key"
 API_secret_key = "API_secret_key"
+#to find your keys go to the development profile in twitter
